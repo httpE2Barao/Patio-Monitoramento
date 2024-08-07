049336015eed9dbb7fc05c695425c8fadd21e307
httpE2Barao <elias.barao@hotmail.com>
2024-08-07 13:42:42 -0300

    criação do projeto

---
+++ my-app/src/componentes/header.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export const Header = () => {
+    return (
+        <>
+            <div>
+                <img src="/images/logo.png" alt="Logo da Pátio"/>
+                    <nav className="bg-black">
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ullam deserunt, magni similique distinctio deleniti accusantium, eaque exercitationem delectus repellat ducimus dolorum voluptate, ad impedit quae molestias! Eum, voluptatibus totam?</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, inventore architecto reiciendis provident odit animi nobis tempora. Inventore explicabo id earum quas repudiandae quasi ab voluptates odio laudantium fugiat! Omnis!</li>
+                    </nav>
+            </div>
+        </>
+    )
+}
