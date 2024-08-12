78847ba590d6324b7cd3f4ceb7400a1b57724e06
httpE2Barao <elias.barao@hotmail.com>
2024-08-12 18:27:06 -0300

    Update botaoSubir.tsx

--- paginadecaptura/src/app/componentes/botaoSubir.tsx
+++ paginadecaptura/src/app/componentes/botaoSubir.tsx
@@ -1,15 +1,15 @@
-import { Box } from "@mui/material"
+import { Box, Button } from "@mui/material"
 import Image from "next/image"
 
 export const BtnToTop = () => {
     return (
-        <Box sx={{ display: "absolute", position: "fixed", width: "70px", height: "70px", right: 20, bottom: 20, cursor: "pointer", backgroundColor: "primary.main", borderRadius: "50%", }}>
+        <Button variant="contained" sx={{ display: "absolute", position: "fixed", width: "70px", height: "70px", right: 20, bottom: 20, cursor: "pointer", backgroundColor: "primary.main", borderRadius: "50%", p: 0 }}>
             <Image src={'/down.png'} alt="Voltar ao topo" width={75} height={75}
-                className="rotate-180 -translate-y-1"
+                className="rotate-180 -translate-y-1 "
                 onClick={() => {
                     document.getElementById('inicio')?.scrollIntoView({ behavior: 'smooth' });
                 }}
             />
-        </Box>
+        </Button >
     )
 }
