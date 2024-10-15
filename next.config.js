898f3f16889edddfbffe5741495ea7226d08ce36
httpE2Barao <elias.barao@hotmail.com>
2024-10-15 16:36:04 -0300

    .

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    typescript: {
-        ignoreBuildErrors: true,
-      },
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-        return config;
-    }
-}
