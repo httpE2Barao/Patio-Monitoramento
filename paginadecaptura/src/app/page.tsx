eb175f2740f0e9f8bd3d52b40bb0100c4713050e
httpE2Barao <elias.barao@hotmail.com>
2024-11-20 17:09:05 -0300

    Update page.tsx

--- paginadecaptura/src/app/page.tsx
+++ paginadecaptura/src/app/page.tsx
@@ -1,34 +1,36 @@
-"use client"
+"use client";
 import { ThemeProvider } from "@mui/material";
 import LoginSignupPage from "componentes/autenticacao/page";
+import { BtnToTop } from "componentes/botaoSubir";
 import FAQ from "componentes/faq";
 import Footer from "componentes/footer";
 import Formulario from "componentes/formulario";
 import Header from "componentes/header";
 import BoasVindas from "componentes/inicio";
 import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { BtnToTop } from "../componentes/botaoSubir";
 import { theme } from "./theme";
 
 const PaginaPrincipal: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginSignupPage />} />
-          <Route path="/form" element={
-            <>
-              <Header />
-              <BoasVindas />
-              <Formulario />
-              <FAQ />
-              <Footer />
-              <BtnToTop />
-            </>
-          } />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Router>
+      {typeof window !== 'undefined' && (
+        <Router>
+          <Routes>
+            <Route path="/auth" element={<LoginSignupPage />} />
+            <Route path="/form" element={
+              <>
+                <Header />
+                <BoasVindas />
+                <Formulario />
+                <FAQ />
+                <Footer />
+                <BtnToTop />
+              </>
+            } />
+            <Route path="*" element={<Navigate to="/auth" replace />} />
+          </Routes>
+        </Router>
+      )}
     </ThemeProvider>
   );
 };
